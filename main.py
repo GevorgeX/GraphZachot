04809bb8d99dce25f23b5c1ee73ad0c29a64f4ab
Gevorg Navoyan <gevnavoyan@gmail.com>
2024-12-22 20:17:23 +0400

    Esim2

--- main.py
+++ main.py
@@ -42,16 +42,20 @@ def colored_graph(graph):
     colors[vertices[0]] = COLOR.BLUE
 
     for key , value in graph.adjacency_list.items():
-        for childs in value:
-            if colors[childs] == COLOR.WHITE:
+        for child in value:
+            if colors[child] == COLOR.WHITE:
                 if colors[key] == COLOR.RED:
-                    colors[childs] = COLOR.BLUE
+                    colors[child] = COLOR.BLUE
                 else:
-                    colors[childs] = COLOR.RED
+                    colors[child] = COLOR.RED
             
-            elif colors[childs] == colors[key]:
-                return (False, colors)
-            
+            elif colors[child] == colors[key]:
+                return (False, (child, key))
+    
+    for i in vertices:
+        if  colors[i] == COLOR.WHITE:
+            colors[i] = COLOR.RED 
+
     return (True, colors)
 
 def draw_bitpart(vertices, edges, colors):
@@ -74,20 +78,65 @@ def draw_bitpart(vertices, edges, colors):
             pos = pos,
             node_color = "red",
             node_size = 1000)
-    plt.savefig("filename.png")
+    plt.savefig(output_name)
 
+def find_circle(grap , vert1 ,vert2):
+    def dfs(node, target, visited, path):
+        visited.add(node)
+        path.append(node)
+        for neighbor in grap[node]:
+            if neighbor == target and len(path) > 2:
+                path.append(neighbor)
+                return True
+            if neighbor not in visited:
+                if dfs(neighbor, target, visited, path):
+                    return True
+
+        path.pop()
+        return False
+
+    # Find a cycle starting from v1 that contains v2
+    for start in [vert1, vert2]:
+        visited = set()
+        path = []
+        if dfs(start, start, visited, path) and vert1 in path and vert2 in path:
+            return path[1:]
+
+    return None
+
+def draw_graph_with_circle(vertices, edges, circle):
+    G = nx.Graph()
+    G.add_nodes_from(vertices)
+
+    for e in edges:
+        G.add_edge(*tuple(e))
+
+    color_map = ["red" if i in circle else "blue" for i in vertices ]
+    pos = nx.spring_layout(G)
+
+    nx.draw(G , 
+            with_labels = True, 
+            pos=pos,
+            node_color = color_map,
+
+            node_size = 400)
+    plt.savefig(output_name)
 
 if __name__ == "__main__":
-    with open('myjson.json', 'r') as f:
+    file_name = input("Get file name(json): ")
+    with open(file_name, 'r') as f:
         file = json.load(f)
     edges = file["E"]
     vertices = file["V"]
     graph = create_graph(vertices, edges)
 
-    isBin,colors  = colored_graph(graph)
-
+    
+    output_name = input("Output png name: ")
+    isBin,res  = colored_graph(graph)
     if isBin:
-        draw_bitpart(vertices, edges, colors)
+        print("Yes your graph is bitpart")
+        draw_bitpart(vertices, edges, res)
     else:
-        # TODO
-        pass
+        print(f"No your graph is not bitpart")
+        circle = find_circle(graph.adjacency_list , res[0] ,res[1])
+        draw_graph_with_circle(vertices, edges, circle)
